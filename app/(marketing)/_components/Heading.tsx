66575397a1d59d237f302f530bff7ff097eabc56
Ali Bin Naseer <aliisinsane0089@gmail.com>
2023-10-31 12:10:13 +0500

    Initial Commit

---
+++ app/(marketing)/_components/Heading.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React from "react";
+
+function Heading() {
+  return (
+    <div className="max-w-3xl space-y-4">
+      <h1>
+        Your Ideas, Documents, & Plans. Unified. Welcome to{" "}
+        <span className="underline">Zotion</span>
+      </h1>
+    </div>
+  );
+}
+
+export default Heading;
