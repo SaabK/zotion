c33c96a2474491a38e1880532a5fb5cfa6991914
Ali Bin Naseer <aliisinsane0089@gmail.com>
2023-11-20 20:30:15 +0500

    Documents Database connected + New Page Button Working

--- app/(main)/_components/Navigation.tsx
+++ app/(main)/_components/Navigation.tsx
@@ -1,11 +1,16 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { ChevronsLeft, MenuIcon } from "lucide-react";
+import { ChevronsLeft, MenuIcon, PlusCircle } from "lucide-react";
 import { usePathname } from "next/navigation";
 import React, { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import UserItem from "./UserItem";
+
+import { useQuery, useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import Item from "./Item";
+import { toast } from "sonner";
 
 function Navigation() {
   const isMobile = useMediaQuery("(max-width: 768px)");
@@ -17,6 +22,9 @@ function Navigation() {
 
   const [isResetting, setIsResetting] = useState(false);
   const [isCollapsed, setIsCollapsed] = useState(isMobile);
+
+  const documents = useQuery(api.documents.get);
+  const create = useMutation(api.documents.create);
 
   // If switched onto mobile size, then the sidebar is going to collapse itself.
   useEffect(() => {
@@ -102,6 +110,16 @@ function Navigation() {
     }
   };
 
+  const handleCreate = () => {
+    const promise = create({ title: "Untitled" });
+
+    toast.promise(promise, {
+      loading: "Creating a new note...",
+      success: "New note created!",
+      error: "Failed to create a new note",
+    });
+  };
+
   return (
     <>
       <aside
@@ -124,9 +142,12 @@ function Navigation() {
         </div>
         <div>
           <UserItem />
+          <Item onClick={handleCreate} label="New Page" icon={PlusCircle} />
         </div>
         <div className="mt-4">
-          <p>Documents</p>
+          {documents?.map((document) => (
+            <p key={document._id}>{document.title}</p>
+          ))}
         </div>
         {/* The little effect on the right side for expanding the navigation */}
         <div
